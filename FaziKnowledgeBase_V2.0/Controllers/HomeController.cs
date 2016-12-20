3999ab544bc757cd1a1819e65de9cd6c811feff1
ITS-Zah <96andriy@ukr.net>
2016-12-20 10:42:08 +0200

    HTML 5

--- FaziKnowledgeBase_V2.0/Controllers/HomeController.cs
+++ FaziKnowledgeBase_V2.0/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace FaziKnowledgeBase_V2._0.Controllers
                 string fileName = System.IO.Path.GetFileName(upload.FileName);
                 // сохраняем файл в папку Files в проекте
                 upload.SaveAs(Server.MapPath("~/Files/" + fileName));
+                //string FKB = WebApplication1.FuzzyLogicBase.FuzzyKnowledgeBase.runFuzzy("Стан екології", Parser("Повітря:Грунти"), Parser("0,6:0,5"), "xls", @"C:\Metagraph_docs\Молоко.xls");
             }
             return RedirectToAction("Index");
         }
