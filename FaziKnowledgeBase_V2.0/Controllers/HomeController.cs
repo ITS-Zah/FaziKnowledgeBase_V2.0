d34dcbad2af0a23ead0c99906c0859e342021c16
ITS-Zah <96andriy@ukr.net>
2017-02-17 15:44:33 +0200

    Тест зчитування

--- FaziKnowledgeBase_V2.0/Controllers/HomeController.cs
+++ FaziKnowledgeBase_V2.0/Controllers/HomeController.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
-using FaziKnowledgeBase_V2._0.Models;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 
 namespace FaziKnowledgeBase_V2._0.Controllers
 {
---
+++ FaziKnowledgeBase_V2.0/Controllers/СonclusionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using FaziKnowledgeBase_V2._0.Models;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace FaziKnowledgeBase_V2._0.Controllers
+{
+    public class СonclusionController : Controller
+    {
+        // GET: Сonclusion
+        public ActionResult Index()
+        {
+            using (FileStream fs = new FileStream("~/Files/" + "FKB.txt", FileMode.OpenOrCreate))
+            {
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(FuzzyKnowledgeBase));
+                FuzzyKnowledgeBase FKB = (FuzzyKnowledgeBase)jsonFormatter.ReadObject(fs);
+            }
+                return View();
+        }
+    }
+}
