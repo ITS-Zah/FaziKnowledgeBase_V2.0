fccd9e95ae14aa89b7a845e4cb7ac5332a134f69
Ivan Ishchenko <ivanishchenkoo@gmail.com>
2017-05-11 01:31:38 +0300

    model of the experiment was added

---
+++ FaziKnowledgeBase_V2.0/Models/DataStructures/Experiment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaziKnowledgeBase_V2._0.Models.DataStructures
+{
+    public class Experiment
+    {
+        private List<double> xPart;
+
+        private double yPart;
+
+        public Experiment()
+        {
+        }
+
+        public Experiment(double y, List<double> x)
+        {
+            yPart = y;
+            xPart = x;
+        }
+
+        public List<double> getXPart()
+        {
+            return xPart;
+        }
+
+        public double getYPart()
+        {
+            return yPart;
+        }
+
+        public void setXPart(List<double> x)
+        {
+            xPart = x;
+        }
+
+        public void setYPart(double y)
+        {
+            yPart = y;
+        }
+
+    }
+}
