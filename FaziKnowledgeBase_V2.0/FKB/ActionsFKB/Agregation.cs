685c5e846995e7aec175fac160d5b5181b89f8a1
Roman <Roman@DESKTOP-015PSEH>
2018-05-29 22:44:43 +0300

    Roman

--- FaziKnowledgeBase_V2.0/FKB/ActionsFKB/Agregation.cs
+++ FaziKnowledgeBase_V2.0/FKB/ActionsFKB/Agregation.cs
@@ -6,27 +6,27 @@ using FuzzyKnowledgeBase_V2._0.Models;
 
 namespace FuzzyKnowledgeBase_V2._0.ActionsFKB
 {
-    public class Agregation 
+    public class Agregation
     {
         public static void AgregationStart(FuzzyKnowledgeBase fkb)
         {
-            for(int i =0; i < fkb.ListOfRule.Count; i++)//по всім правилам
+            for (int i = 0; i < fkb.ListOfRule.Count; i++)//по всім правилам
             {
-                double minvalue = Double.PositiveInfinity;
+                double minvalue = 1;
                 for (int j = 0; j < fkb.ListOfRule[i].Antecedents.Count; j++)//по всім параметрам правила
                 {
-                    if(/*fkb.ListOfRule[i].Antecedents[j].ZnachFp < minvalue &&*/ fkb.ListOfRule[i].Antecedents[j].ZnachFp != 0)
+                    if (/*fkb.ListOfRule[i].Antecedents[j].ZnachFp < minvalue &&*/ fkb.ListOfRule[i].Antecedents[j].ZnachFp != 0)
                     {
                         minvalue = minvalue * fkb.ListOfRule[i].Antecedents[j].ZnachFp;
                     }
                 }
-                if(minvalue == Double.PositiveInfinity)
+                if (minvalue == 1)
                 {
                     minvalue = 0;
                 }
                 fkb.ListOfRule[i].MinZnach = minvalue;
             }
-            
+
         }
     }
 }
